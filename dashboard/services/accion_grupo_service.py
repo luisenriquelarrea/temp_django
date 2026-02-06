45dfbd9d7e53937843966a1468f4a5c934d614e7
luisenriquelarrea <enrique.larrea.mx@outlook.com>
2026-02-06 15:20:30 -0600

    add methods get allowed table actions, navbar

--- dashboard/services/accion_grupo_service.py
+++ dashboard/services/accion_grupo_service.py
@@ -2,7 +2,7 @@ from dashboard.models import AccionGrupo
 
 def get_allowed_breadcrumbs_for_group(user, seccionMenuId):
     """
-    Returns AccionGrupo queryset for breadcrumbs allowed to a user
+    Returns AccionGrupo queryset for breadcrumbs allowed to a group
     """
     grupos = user.groups.all()
 
@@ -20,9 +20,29 @@ def get_allowed_breadcrumbs_for_group(user, seccionMenuId):
         )
     )
 
+def get_allowed_navbar_for_group(user, seccionMenuId):
+    """
+    Returns AccionGrupo queryset for navbar allowed to a group
+    """
+    grupos = user.groups.all()
+
+    return (
+        AccionGrupo.objects
+        .filter(
+            grupo__in=grupos,
+            status=True,
+            accion__status=True,
+            accion__on_navbar=True,
+            accion__seccion_menu_id=seccionMenuId,
+        )
+        .select_related(
+            "accion",
+        )
+    )
+
 def get_allowed_menus_for_group(user):
     """
-    Returns AccionGrupo queryset for menus allowed to a user
+    Returns AccionGrupo queryset for menus allowed to a group
     """
     grupos = user.groups.all()
 
@@ -44,5 +64,25 @@ def get_allowed_menus_for_group(user):
         .order_by(
             "accion__seccion_menu__menu__descripcion",
             "accion__seccion_menu__navbar_label",
+        )
+    )
+
+def get_allowed_table_actions_for_group(user, seccionMenuId):
+    """
+    Returns AccionGrupo queryset for table actions allowed to a group
+    """
+    grupos = user.groups.all()
+
+    return (
+        AccionGrupo.objects
+        .filter(
+            grupo__in=grupos,
+            status=True,
+            accion__status=True,
+            accion__on_table=True,
+            accion__seccion_menu_id=seccionMenuId,
+        )
+        .select_related(
+            "accion",
         )
     )
--- dashboard/views.py
+++ dashboard/views.py
@@ -10,6 +10,8 @@ from rest_framework.permissions import IsAuthenticated
 from dashboard.services.accion_grupo_service import (
     get_allowed_breadcrumbs_for_group,
     get_allowed_menus_for_group,
+    get_allowed_navbar_for_group,
+    get_allowed_table_actions_for_group,
 )
 
 from .models import AccionBasica, Menu, SeccionMenu
@@ -61,12 +63,48 @@ class AccionGrupoViewSet(viewsets.ModelViewSet):
 
         serializer = AccionGrupoActionsSerializer(acciones, many=True)
         return Response(serializer.data)
+    
+    @action(detail=False, methods=["post"], url_path="allowed_navbar")
+    def allowed_navbar(self, request):
+        seccion_menu_id = request.data.get("seccionMenuId")
+
+        if not seccion_menu_id:
+            return Response(
+                {"detail": "seccionMenuId is required"},
+                status=400
+            )
+
+        acciones = get_allowed_navbar_for_group(
+            request.user,
+            seccion_menu_id
+        )
+
+        serializer = AccionGrupoActionsSerializer(acciones, many=True)
+        return Response(serializer.data)
 
     @action(detail=False, methods=["get"], url_path="allowed_menus")
     def allowed_menus(self, request):
         acciones = get_allowed_menus_for_group(request.user)
 
         serializer = AccionGrupoSeccionMenuSerializer(acciones, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=["post"], url_path="allowed_table_actions")
+    def allowed_table_actions(self, request):
+        seccion_menu_id = request.data.get("seccionMenuId")
+
+        if not seccion_menu_id:
+            return Response(
+                {"detail": "seccionMenuId is required"},
+                status=400
+            )
+
+        acciones = get_allowed_table_actions_for_group(
+            request.user,
+            seccion_menu_id
+        )
+
+        serializer = AccionGrupoActionsSerializer(acciones, many=True)
         return Response(serializer.data)
 
 class MenuViewSet(viewsets.ModelViewSet):
