14b612fcc2534197b975a8dc3ce9725b02a90a81
luisenriquelarrea <enrique.larrea.mx@outlook.com>
2026-02-06 13:23:42 -0600

    RFX get_allowed_menus_for_useRFX get_allowed_menus_for_user

--- dashboard/services/accion_grupo_service.py
+++ dashboard/services/accion_grupo_service.py
@@ -11,11 +11,18 @@ def get_allowed_menus_for_user(user):
         .filter(
             grupo__in=grupos,
             status=True,
-            accion__status=True
+            accion__status=True,
+            accion__on_breadcrumb=True,
+            accion__seccion_menu__status=True,
+            accion__seccion_menu__menu__status=True,
         )
         .select_related(
             "accion",
             "accion__seccion_menu",
             "accion__seccion_menu__menu"
+        )
+        .order_by(
+            "accion__seccion_menu__menu__descripcion",
+            "accion__seccion_menu__navbar_label",
         )
     )
