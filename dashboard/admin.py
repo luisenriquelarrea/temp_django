5b6ffba741e0a5f1f1ebed7a111ae60f75b6f62f
luisenriquelarrea <enrique.larrea.mx@outlook.com>
2026-02-06 18:52:51 -0600

    RFX admin site set display columns

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -6,13 +6,81 @@ from .models import (
     Menu,
     SeccionMenu,
     SeccionMenuInput,
-    StyledColumn,
 )
 
-admin.site.register(AccionBasica)
-admin.site.register(Accion)
-admin.site.register(AccionGrupo)
-admin.site.register(Menu)
-admin.site.register(SeccionMenu)
-admin.site.register(SeccionMenuInput)
-admin.site.register(StyledColumn)
+@admin.register(AccionBasica)
+class AccionBasicaAdmin(admin.ModelAdmin):
+    list_display = (
+        "descripcion",
+        "label",
+        "icon",
+        "on_breadcrumb",
+        "on_navbar",
+        "on_table",
+        "status",
+    )
+
+@admin.register(Accion)
+class AccionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "seccion_menu",
+        "descripcion",
+        "label",
+        "icon",
+        "on_breadcrumb",
+        "on_navbar",
+        "on_table",
+        "status",
+    )
+
+@admin.register(AccionGrupo)
+class AccionGrupoAdmin(admin.ModelAdmin):
+    list_display = (
+        "grupo",
+        "get_seccion_menu",
+        "accion",
+        "status",
+    )
+
+    def get_seccion_menu(self, obj):
+        return obj.accion.seccion_menu
+
+    get_seccion_menu.short_description = "Sección menú"
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "descripcion",
+        "label",
+        "icon",
+        "orden",
+        "status",
+    )
+
+@admin.register(SeccionMenu)
+class SeccionMenuAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "menu",
+        "descripcion",
+        "navbar_label",
+        "icon",
+        "status",
+    )
+
+@admin.register(SeccionMenuInput)
+class SeccionMenuInputAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "seccion_menu",
+        "input_type",
+        "input_label",
+        "input_id",
+        "input_name",
+        "orden",
+        "status",
+    )
+
+# admin.site.register(StyledColumn)
